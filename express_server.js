2c14803d1aba35f2f626395cdbfc3f434143d483
Tony Yu <tony.kwangjong.yu@gmail.com>
2022-03-22 04:08:09 +0000

    generate new string of 6

--- express_server.js
+++ express_server.js
@@ -1,13 +1,20 @@
 const express = require("express"); //imported express
+const bodyParser = require("body-parser"); //imported body parser
 const app = express(); //setting app to the express function 
 const PORT = 8080; // default port 8080
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+function generateRandomString() {
+  return Math.random().toString(36).slice(-6);
+}
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
 
 app.get("/", (req, res) => { // req = request res = response
   res.send("Hello!");
@@ -26,11 +33,19 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars)
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = {shortURL: req.params.shortURL, longURL: req.params.longURL};
+  const templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("Ok");
+})
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
