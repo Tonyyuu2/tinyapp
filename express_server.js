6e08f10f8f8c39e3759f79fdfad119e9f715cf3f
Tony Yu <tony.kwangjong.yu@gmail.com>
2022-03-22 17:11:40 +0000

    add comments to remind self of express functionality

--- express_server.js
+++ express_server.js
@@ -3,10 +3,10 @@ const bodyParser = require("body-parser"); //imported body parser
 const app = express(); //setting app to the express function 
 const PORT = 8080; // default port 8080
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true})); //using body parser for urlencoding?
+app.set("view engine", "ejs"); //setting the view engine to ejs 
 
-function generateRandomString() {
+function generateRandomString() { // generates random alphanumeric characters
   return Math.random().toString(36).slice(-6);
 }
 
@@ -17,37 +17,37 @@ const urlDatabase = {
 
 
 app.get("/", (req, res) => { // req = request res = response
-  res.send("Hello!");
+  res.send("Hello!"); //sends hello to the browser
 });
 
 app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+  res.json(urlDatabase); //gets the json response of the urldatabase object
 });
 
-app.get("/hello", (req, res) => {
+app.get("/hello", (req, res) => { //on the local host/hello, send this...
   res.send("<html><body>Hello <b>World</b></body></html>\n")
 });
 
-app.get("/urls", (req, res) => {
+app.get("/urls", (req, res) => { // on the urls page, response, I want to render with ejs and post 
   const templateVars = { urls: urlDatabase};
-  res.render("urls_index", templateVars)
+  res.render("urls_index", templateVars) //render using urls_index for the value of templateVars
 });
 
-app.get("/urls/new", (req, res) => {
+app.get("/urls/new", (req, res) => { //get the response for urls/new and I want to render what I have on urls_new
   res.render("urls_new");
 });
 
-app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
-  res.render("urls_show", templateVars);
+app.get("/urls/:shortURL", (req, res) => { //when given a shortURL 
+  const templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]}; // need clarification 
+  res.render("urls_show", templateVars); // I want to render urls_show with templateVars
 });
 
-app.get("/u/:shortURL", (req, res) => {
+app.get("/u/:shortURL", (req, res) => { // need clarification 
  const longURL = urlDatabase[req.params.shortURL]
   res.redirect(longURL)
 })
 
-app.post("/urls", (req, res) => {
+app.post("/urls", (req, res) => { // need clarification 
   const longURL = req.body.longURL
   const shortURL = generateRandomString()
   urlDatabase[shortURL] = longURL;
@@ -57,6 +57,6 @@ app.post("/urls", (req, res) => {
 
 
 
-app.listen(PORT, () => {
+app.listen(PORT, () => { //telling server to listen to this port 
   console.log(`Example app listening on port ${PORT}!`);
 });
