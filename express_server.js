26423a1cfdd5ea040b46a087c3806fc71ea5a2c4
Tony Yu <tony.kwangjong.yu@gmail.com>
2022-03-25 00:21:44 +0000

    add security features and edge cases

--- express_server.js
+++ express_server.js
@@ -27,11 +27,29 @@ const userLookUp = (email, users) => {
   return false
 };
 
-const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+const urlsForUser = (id, database) => {
+  let userUrls = {}; 
+  if(!id) {
+    return null
+  }
+  for (const shortURL in database) {
+    if (database[shortURL].userID === id) {
+      userUrls[shortURL] = database[shortURL]
+    }
+  }
+  return userUrls;
 };
 
+const urlDatabase = {
+  b6UTxQ: {
+    longURL: "https://www.tsn.ca",
+    userID: "aJ48lW"
+  },
+  i3BoGr: {
+    longURL: "https://www.google.ca",
+    userID: "aJ48lW"
+  }
+};
 const users = {
   "userRandomID": {
     id: "userRandomID", 
@@ -43,87 +61,108 @@ const users = {
     email: "user2@example.com", 
     password: "6789"
   }
-};
-
-        //"/" area is route area // root directory
+};      //"/" area is route area // root directory
 app.get("/", (req, res) => { // req = request res = response
   res.send("Hello!"); //sends hello to the browser
 });
-
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase); //gets the json response of the urldatabase object
 });
-
 app.get("/hello", (req, res) => { //on the local host/hello, send this...
   res.send("<html><body>Hello <b>World</b></body></html>\n")
 });
-
 app.get("/urls", (req, res) => { // on the urls page, response, I want to render with ejs and post 
   const user = users[req.cookies["user_id"]]
   const templateVars = { 
     user,
-    urls: urlDatabase
+    urls: urlsForUser(user, urlDatabase)
   };
   res.render("urls_index", templateVars) //render using urls_index for the value of templateVars
 });
-
 app.get("/urls/new", (req, res) => { //get the response for urls/new and I want to render what I have on urls_new
   const user = users[req.cookies["user_id"]]
-  const templateVars = { 
+  if(!user) {
+    return res.redirect("/login")
+  }
+  const templateVars = {
     user
   };
-    res.render("urls_new", templateVars);
+  res.render("urls_new", templateVars);
   });
-
 app.get("/urls/:shortURL", (req, res) => { //when given a shortURL 
   const user = users[req.cookies["user_id"]]
+  if(!user) {
+    return res.send(404)
+  }
   const templateVars = {
     shortURL: req.params.shortURL, // when you do the get req. the req is the object of the url. params is one of the keys and :shortURL is the value of that key
-    longURL: urlDatabase[req.params.shortURL],
+    longURL: urlDatabase[req.params.shortURL].longURL,
     user
   }; // need clarification  
   res.render("urls_show", templateVars); // I want to render urls_show with templateVars 
 }); //shortURL sets those values that we need to have and then get passed on 
-        //is the path :id the colon is a placeholder.
+//is the path :id the colon is a placeholder. 
 app.get("/u/:shortURL", (req, res) => { // need clarification 
-  const longURL = urlDatabase[req.params.shortURL]
+  const user = users[req.cookies["user_id"]]
+  if(!user) {
+    return res.send(404)
+  }
+  if (!urlDatabase[req.params.shortURL]) {
+    return res.send(404)
+  }
+  const longURL = urlDatabase[req.params.shortURL].longURL
   res.redirect(longURL)
 });
-
-app.post("/urls", (req, res) => { // need clarification 
+app.post("/urls", (req, res) => { // need clarification
+  const user = users[req.cookies["user_id"]]
+  if(!user) {
+    return res.send("Uh-oh... Are you logged in?")
+  }
   const longURL = req.body.longURL
-  const shortURL = generateRandomString()
-  urlDatabase[shortURL] = longURL;
-  res.redirect(`/urls/${shortURL}`);
-});
+  const shortURL = generateRandomString() 
+  urlDatabase[shortURL] = {longURL: req.body.longURL, userID: user};
 
+  //the value of longURL that's in shortURL in the ulrdatabase object
+  res.redirect(`/urls/${shortURL}`);
+}); // cannot test post requests // get = no body, checking information it already has // post = sending new information to the backend -> processing 
 app.post("/urls/:shortURL/delete", (req, res) => {
+  const user = users[req.cookies["user_id"]]
+  if(!user) {
+    return res.send("Please log in")
+  }
   delete urlDatabase[req.params.shortURL];
   res.redirect('/urls');
 });
-
 app.get("/urls/:shortURL/edit", (req, res) => {
   const user = users[req.cookies["user_id"]] //the user id value in cookies in users object -> all the info with it 
-  const templateVars = {shortURL: req.params.shortURL, 
-    longURL: urlDatabase[req.params.shortURL],
+  if(!user) {
+    return res.send(404)
+  }
+  const templateVars = {
+    shortURL: req.params.shortURL, 
+    longURL: urlDatabase[req.params.shortURL].longURL,
     user
 };
-  res.render("urls_show", templateVars); //render builds a page 
+  res.render("urls_show", templateVars); //render builds a page
 });
-
-app.get("/register", (req, res) => {
+app.get("/register", (req, res) =>  {
   const templateVars = {user: null};
+  const user = users[req.cookies["user_id"]]
+  if(user) {
+    return res.redirect("/urls")
+  }
   res.render("registeration_page", templateVars)
 });
-
 app.get("/login", (req, res) => {
   const user = users[req.cookies["user_id"]]
+  if(user) {
+    return res.redirect("/urls")
+  }
   const templateVars = {
     user
   }
   res.render("login", templateVars)
 });
-
 app.post("/register", (req, res) => { //post expects info from front end // get is backend telling front end 
   const email = req.body.email;
   const password = req.body.password;
@@ -147,23 +186,19 @@ app.post("/register", (req, res) => { //post expects info from front end // get 
   res.redirect("/urls");
 
 });
-
 app.post("/urls/:shortURL/edit", (req, res) => {
   const shortURL = req.params.shortURL;
   const longURL = req.body.longURL;
-  urlDatabase[shortURL] = longURL; //urlDatabase[shorturl] = lo // shortURL is the key and the longURL is the value 
+  urlDatabase[shortURL].longURL = longURL; //urlDatabase[shorturl] = lo // shortURL is the key and the longURL is the value 
   res.redirect("/urls");
 });
 // https://www.youtube.com/c/TraversyMedia/videos
-
 app.post("/login", (req, res) => {
   // const id = req.body["user_id"]; //req.body["user_id"] = email login info 
   const {email, password} = req.body;
-  console.log('users :', users);
   const user = userLookUp(email, users);
- 
   if(!user) {
-    res.send(403)
+    res.send(403) //res.status(400).send(msg)
     return
   } 
   if(user.password !== password) {
@@ -173,16 +208,13 @@ app.post("/login", (req, res) => {
   res.cookie("user_id", user.id); //"user_id" === id === email 
   res.redirect("/urls");
 });
-
 app.get("/logout", (req, res) => {
   res.clearCookie("user_id")
   res.redirect("/urls")
 });
-
 app.listen(PORT, () => { //telling server to listen to this port 
   console.log(`Example app listening on port ${PORT}!`);
 });
-
 //req.params = an object that holds all the parameters from the URL 
 // nodemon is middleware
 // <%= id %> is a dynamic tag that brings in the variable 
